cd9fcc7083cb1be0a82e8c05e68229dcde2c5681
pawchr <pawchr@gmail.com>
2022-11-15 22:06:25 +0100

    Authenttication added

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import DbOperations from './db-operations';
 import reportWebVitals from './reportWebVitals';
+import Authentication from './Authentication';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <DbOperations />
+    <Authentication />
   </React.StrictMode>
 );
 
